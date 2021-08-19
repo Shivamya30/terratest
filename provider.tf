93d5a9a6675346b32b076d0faff012430ebc559b
SHIVAMYA SINHA <42280946+Shivamya30@users.noreply.github.com>
2021-08-19 19:13:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-     access_key = "AKIARDMMFO2SEUD5QO7Y"
-     secret_key = "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"
+     access_key = "****************"
+     secret_key = "**************************"
      region = "us-east-2"
 }
 data "aws_availability_zones" "all" {}
