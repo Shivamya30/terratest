dc0877447710ed6e0745343c44cf898f5a120cdc
SHIVAMYA SINHA <42280946+Shivamya30@users.noreply.github.com>
2021-08-20 17:55:25 +0530

    Update pipeline .groovy

--- pipeline .groovy
+++ pipeline .groovy
@@ -18,27 +18,18 @@ pipeline {
                 git branch: 'main', credentialsId: 'github', url: 'https://github.com/Shivamya30/terratest.git'
             }
         }
-        stage ("terraform init") {
+        stage ("terraform ") {
             steps {
                 sh 'terraform init'
+                sh 'terraform plan '
+                sh 'terraform apply --auto-approve'
+                sh 'terraform destroy --auto-approve'
+                if 
+                
             }
         }
         
-        stage ("terrafrom plan") {
-            steps {
-                sh 'terraform plan '
-            }
-        }
-        stage ("terraform apply") {
-            steps {
-                sh 'terraform apply --auto-approve'
-            }
-        }
-        stage ("terraform destroy"){
-            steps{
-                sh 'terraform destroy --auto-approve'
-            }
-        }
+       
         stage ("terratest apply"){
             steps {
                 withEnv(["PATH+GO=${GOPATH}/bin"]){
